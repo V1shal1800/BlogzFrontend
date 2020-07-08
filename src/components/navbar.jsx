008d24be6378876bad6f7d376610dd41502df510
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-08 23:15:51 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,10 +9,10 @@ class Navbar extends Component {
                 <ul className="navbar-nav">
                 <Router>
                     <li className="nav-item active">
-                        <Link to="/"><span className="nav-link">Home <span className="sr-only">(current)</span></span></Link>
+                        <Link to={"/"}><span className="nav-link">Home <span className="sr-only">(current)</span></span></Link>
                     </li>
                     <li className="nav-item">
-                    <Link to="/admin"><span className="nav-link">Admin</span></Link>
+                    <Link to={"/admin"}><span className="nav-link">Admin</span></Link>
                     </li>
                 </Router>
                 </ul>
