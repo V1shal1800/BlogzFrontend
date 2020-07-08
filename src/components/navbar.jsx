dd244b15b8288012f25a14aa3df5109e8a6068e5
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-09 01:00:23 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,18 +3,16 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 class Navbar extends Component {
     render() { 
-        function refreshPage() { window. location. reload(false);
-        
         return ( 
             <nav className="navbar navbar-dark bg-dark">
                 <span className="navbar-brand m-auto" style={{fontSize: 50}}>Blogz</span>
                 <ul className="navbar-nav">
                 <Router>
                     <li className="nav-item active">
-                        <Link to={"/"}><span className="nav-link"><button onClick={this.refreshPage}>Home </button> <span className="sr-only">(current)</span></span></Link>
+                        <Link to={"/"}><span className="nav-link">Home<span className="sr-only">(current)</span></span></Link>
                     </li>
                     <li className="nav-item">
-                    <Link to={"/admin"}><span className="nav-link"><button onClick={this.refreshPage}>Admin</button></span></Link>
+                    <Link to={"/admin"}><span className="nav-link">Admin</span></Link>
                     </li>
                 </Router>
                 </ul>
@@ -23,4 +21,4 @@ class Navbar extends Component {
     }
 }
  
-export Navbar;
+export default Navbar;
